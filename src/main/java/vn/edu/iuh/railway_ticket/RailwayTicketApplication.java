9da90cbc39211fb79d3ca0cbb4ec37d2a7e0d190
chison2001 <sonsonsonle9a1@gmail.com>
2023-07-25 17:48:48 +0700

    create project

---
+++ src/main/java/vn/edu/iuh/railway_ticket/RailwayTicketApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.railway_ticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwayTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RailwayTicketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/edu/iuh/railway_ticket/RailwayTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.railway_ticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailwayTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
